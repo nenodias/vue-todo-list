18a017246fa11d583e6e70561b4eeba7d4d61721
Horácio Dias <horacio.dias92@gmail.com>
2017-05-09 22:52:28 -0300

    Finalizando o teste o InputTask

--- test/unit/specs/InputTask.spec.js
+++ test/unit/specs/InputTask.spec.js
@@ -1,10 +1,38 @@
 import Vue from 'vue'
 import InputTask from 'src/components/InputTask'
 
+
 describe('InputTask.vue', () => {
-  it('should render correct contents', () => {
+  let vm = {}
+
+  beforeEach(function() {
     const Constructor = Vue.extend(InputTask)
-    const vm = new Constructor().$mount()
+    vm = new Constructor().$mount()
+  })
+
+  it('should render correct contents', () => {
     expect(vm.$el.querySelectorAll('.new-todo').length).to.equal(1)
   })
+  it('should create task correctly', () => {
+      let task = vm.createTask('Comprar leite')
+      expect(task.title).to.equal('Comprar leite')
+  })
+  it('should create the task not completed', () => {
+      let task = vm.createTask('Comprar leite')
+      expect(task.completed).to.be.false
+  })
+  it('should clean the input', () => {
+      vm.$el.querySelector('.new-todo').value = 'Comprar leite'
+      vm.clearField()
+      expect(vm.$el.querySelector('.new-todo').value).to.equal('')
+
+  })
+
+  it('should call the event', () => {
+      let spy = sinon.spy()
+      vm.$on('newTask', spy)
+      vm.broadcast()
+      expect(spy).to.have.been.called
+  })
+
 })
